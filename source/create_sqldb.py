577e6f8f426ee22e01ecd76c68c25358650e73e9
Belen Michel <belenmichel@MacBook-Pro-de-Belen.local>
2019-05-07 21:38:04 -0500

    create db to be improved

--- source/create_sqldb.py
+++ source/create_sqldb.py
@@ -1,18 +1,47 @@
+import sqlite3
+import os
+import csv
+import pandas as pd
+
+
+def clean_str(col_names):
+    '''
+        Removes special characters from a string.
+        input:
+        col_names: string with column names
+        output:
+        sting where special characters where removed
+        '''
+    #TODO: replace this ad-hoc function with a re function.
+    col_names = col_names.replace('.','')
+    col_names = col_names.replace('#','')
+    col_names = col_names.replace('-','')
+    col_names = col_names.replace('(','')
+    col_names = col_names.replace(')','')
+    col_names = col_names.replace('&','')
+    col_names = col_names.replace('/','')
+    col_names = col_names.replace(':','')
+    col_names = col_names.replace(' ','')
+    col_names = col_names.replace('__','')
+    return col_names
+
+
+data_dir = "../ncdoc_data/data/preprocessed"
 con = sqlite3.connect(os.path.join(data_dir, "inmates.db"))
 cur = con.cursor()
 
-tables = ['INMT4AA1', 'INMT4BB1', 'INMT9CF1',\
-          'OFNT1BA1', 'OFNT3BB1', 'OFNT3CE1',\
-          'OFNT3DE1', 'INMT4CA1']
+tables =  ['INMT4AA1', 'INMT4BB1', 'INMT9CF1',\
+           'OFNT1BA1', 'OFNT3BB1', 'OFNT3CE1',\
+           'OFNT3DE1', 'INMT4CA1']
 
 for table in tables:
-    #print(table)
-    file_name = os.path.join(data_dir, "preprocessed/{}.csv".format(table)) 
+    print(table)
+    file_name = os.path.join(data_dir, "{}.csv".format(table))
     col_names = pd.read_csv(file_name, nrows=0).columns
     n_columns = len(col_names)
     col_names = clean_str(', '.join(col_names))
     cur.execute('DROP TABLE IF EXISTS {}'.format(table))
-    cur.execute("CREATE TABLE {} ({});".format(table, col_names))
+    cur.execute('CREATE TABLE {} ({});'.format(table, col_names))
     
     #File contains NULL bytes. That's why I replaced '\0' with ''
     reader = csv.reader(x.replace('\0','') for x in open(file_name))
@@ -22,3 +51,4 @@ for table in tables:
 
 con.commit()
 con.close()
+
