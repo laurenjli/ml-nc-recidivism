09529db5407ae188e36df518759ea8bf391dad4f
Lauren Li <laurenli@cs.uchicago.edu>
2019-05-08 19:15:55 -0500

    updating createsqldb.py

--- source/create_sqldb.py
+++ source/create_sqldb.py
@@ -30,15 +30,16 @@ data_dir = "../ncdoc_data/data/preprocessed"
 con = sqlite3.connect(os.path.join(data_dir, "inmates.db"))
 cur = con.cursor()
 
-tables =  ['INMT4AA1', 'INMT4BB1', 'INMT9CF1',\
-           'OFNT1BA1', 'OFNT3BB1', 'OFNT3CE1',\
-           'OFNT3DE1', 'INMT4CA1']
+tables =  ['OFNT3CE1','INMT4AA1', 'INMT4BB1', 'INMT9CF1',\
+           'OFNT1BA1', 'OFNT3BB1', 
+           'OFNT3DE1', 'INMT4CA1'] 
 
 for table in tables:
     print(table)
     file_name = os.path.join(data_dir, "{}.csv".format(table))
     col_names = pd.read_csv(file_name, nrows=0).columns
     n_columns = len(col_names)
+    print(n_columns)
     col_names = clean_str(', '.join(col_names))
     cur.execute('DROP TABLE IF EXISTS {}'.format(table))
     cur.execute('CREATE TABLE {} ({});'.format(table, col_names))
@@ -47,6 +48,9 @@ for table in tables:
     reader = csv.reader(x.replace('\0','') for x in open(file_name))
     for row in reader:
         row = [None if x == '' else x for x in row]
+        if len(row) < n_columns:
+            print('DROPPED ROW: {}'.format(row))
+            continue
         cur.execute("INSERT INTO {} VALUES ({});".format(table,",".join(['?']*n_columns)), row)
 
 con.commit()
