dfe298c6b2a2f9b76176330d31e44c9974217db0
unknown <rachelker@uchicago.edu>
2019-05-29 09:55:34 -0500

    functions to add features

--- source/add_features_rachel.py
+++ source/add_features_rachel.py
@@ -1,9 +1,6 @@
 '''
-Code to combine loading database, getting labels and features
-and temporal validation
-
-Before using this file, you need the downloaded CSV data for NC recidivism from https://github.com/jtwalsh0/ncdoc_data
-The data we are using for this analysis was downloaded as of 26 April 2019 
+Code for creating features
+Rachel Ker
 '''
 
 import gettinglabels
@@ -11,127 +8,92 @@ from create_sqldb import create_db
 
 import sqlite3
 
+DATABASE_FILENAME="../ncdoc_data/data/preprocessed/inmates.db"
 
-# Edit the following directory based on your data locations
-CSVFOLDER="../ncdoc_data/data/preprocessed/"
-DATABASE_FILENAME=CSVFOLDER + "inmates.db"
-
-
-def setup():
-    '''
-    Load datasets into inmates.db and generate new table with labels
-    '''
-    create_db(CSVFOLDER)
-
-
-## EDITED THIS FUNCTION TO GENERATE NEW FEATURES
-def add_features(database_path=DATABASE_FILENAME):
+def add_first_feature(database_path=DATABASE_FILENAME):
     '''
     Creating a new table with features and labels
     '''    
+    # First feature - how long they have been incarcerated in days
     feature_query = (
         """
         WITH incarceration_len as(
-                SELECT ID, PREFIX, START_DATE, END_DATE, 
-                julianday(END_DATE)-julianday(START_DATE) as INCARCERATION_LEN_DAYS
-                FROM labels
-            ),
-            totcntavg_incarceration_allprior as (
-                SELECT a.ID, a.PREFIX, a.START_DATE, a.END_DATE, 
-                sum(b.INCARCERATION_LEN_DAYS) as TOTAL_INCARCERATION_ALLPRIOR,
-                count(*) as NUM_PREV_INCARCERATION_ALLPRIOR,
-                sum(b.INCARCERATION_LEN_DAYS)/count(*) as AVG_INCARCERATION_ALLPRIOR 
-                FROM incarceration_len as a join incarceration_len as b on a.ID=b.ID
-                WHERE b.END_DATE <= a.END_DATE 
-                GROUP BY a.ID, a.PREFIX, a.START_DATE, a.END_DATE
-            ),
-            totcntavg_incarceration_last5yr as (
-                SELECT a.ID, a.PREFIX, a.START_DATE, a.END_DATE, 
-                sum(b.INCARCERATION_LEN_DAYS) as TOTAL_INCARCERATION_LAST5YR,
-                count(*) as NUM_PREV_INCARCERATION_LAST5YR,
-                sum(b.INCARCERATION_LEN_DAYS)/count(*) as AVG_INCARCERATION_LAST5YR  
-                FROM incarceration_len as a join incarceration_len as b on a.ID=b.ID 
-                WHERE julianday(b.END_DATE) >= (julianday(a.END_DATE) - 1825)
-                GROUP BY a.ID, a.PREFIX, a.START_DATE, a.END_DATE
-            ),
-            relative_incarceration_len as (
-                SELECT ID, PREFIX, START_DATE, END_DATE, 
-                INCARCERATION_LEN_DAYS - (SELECT
-                sum(b.INCARCERATION_LEN_DAYS)/count(*) as mean
-                FROM incarceration_len as a, incarceration_len as b
-                WHERE b.END_DATE <= a.END_DATE) as RELATIVE_INCARCERATION_LEN_DAYS
-                from incarceration_len
-            )
-
+            SELECT ID, PREFIX, julianday(END_DATE)-julianday(START_DATE) as INCARCERATION_LEN_DAYS
+            FROM labels
+        )
         SELECT * 
-        FROM labels 
-        natural join incarceration_len 
-        natural join totcntavg_incarceration_allprior 
-        natural join totcntavg_incarceration_last5yr
-        natural join relative_incarceration_len
+        FROM labels natural join incarceration_len
         """)
 
     gettinglabels.query_db(feature_query, args=None, database_path=DATABASE_FILENAME, table_name='data', new_table=True)
-    print('feature created')
+    print('first feature created')
 
 
-def temporal_validation(csv_name, train_start_year, test_start_year, time_period=365.0):
-    '''
-    Splitting up train and test sets based on the year specified
-    e.g. train 2015 using 2016 to get labels (defined by time period),
-    test on 2017
+def add_new_features(database_path, new_features_list, insert_query_list):
 
-    To implement: 
-        Train data - from train start year to (test start year - timeperiod)
-        filter data for end year to be in test set
-    '''
+    # connect to database to add more features
+    con = sqlite3.connect(database_path)
+    cur = con.cursor()
 
-    train_query = """
-    SELECT * FROM data
-    WHERE END_DATE >= ?
-    AND julianday(END_DATE) < julianday(?) - ?
-    """
-    train_args = (str(train_start_year)+'-01-01', str(test_start_year)+'-01-01', time_period)
-
-    gettinglabels.query_db(train_query, train_args, DATABASE_FILENAME, 
-                           table_name='traindata', new_table=True, 
-                           csv_filename=CSVFOLDER + 'traintest/' + csv_name + '_train.csv')
-    print("created {} train set".format(csv_name))
-
-    test_query = """
-    SELECT * FROM data
-    WHERE END_DATE LIKE ?
-    """
-    test_args = (str(test_start_year)+'%',)
-        
-    gettinglabels.query_db(test_query, test_args, DATABASE_FILENAME, 
-                           table_name='testdata', new_table=True, 
-                           csv_filename=CSVFOLDER + 'traintest/' + csv_name+ '_test.csv')
-    print("created {} test set".format(csv_name))
+    # add columns for new features
+    create_new_col = ''''''
+    for new in new_features_list:
+        cur.execute('ALTER TABLE data ADD COLUMN {}'.format(new))
+        print('added column {}'.format(new))
+    
+    # insert values into new columns
+    for q in insert_query_list:
+        cur.execute(q)
+        print('insert done')
+    
+    con.commit()
+    con.close()
 
 
-def get_train_test_splits(train_start_year=1995, test_start_year=1997, time_period=365.0):
-    '''
-    Gets the train data and test data for individual train-test periods
-    1) traindata table and test data table in db
-    2) write out as csv files in data/preprocessed/traintest/ folder
-    '''
-    gettinglabels.create_labels(DATABASE_FILENAME, time_period=time_period, default_max = 10000.0, table_name = 'labels')
-    add_features()
-    temporal_validation('test_'+ str(test_year), train_start_year=train_start_year, test_start_year=test_start_year, time_period=time_period)
+def add_incarceration_lens():
+
+    new_features = ['TOTAL_INCARCERATION_ALLPRIOR', 'NUM_PREV_INCARCERATION_ALLPRIOR', 'AVG_INCARCERATION_ALLPRIOR',
+                    'TOTAL_INCARCERATION_LAST5YR', 'NUM_PREV_INCARCERATION_LAST5YR', 'AVG_INCARCERATION_LAST5YR',
+                    'RELATIVE_INCARCERATION_LEN_DAYS']
+
+    query = (
+        '''
+        INSERT INTO data(TOTAL_INCARCERATION_ALLPRIOR, NUM_PREV_INCARCERATION_ALLPRIOR, AVG_INCARCERATION_ALLPRIOR)
+        SELECT 
+        sum(b.INCARCERATION_LEN_DAYS) as TOTAL_INCARCERATION_ALLPRIOR,
+        count(*) as NUM_PREV_INCARCERATION_ALLPRIOR,
+        sum(b.INCARCERATION_LEN_DAYS)/count(*) as AVG_INCARCERATION_ALLPRIOR 
+        FROM data as a join data as b on a.ID=b.ID
+        WHERE b.END_DATE <= a.END_DATE 
+        GROUP BY a.ID, a.PREFIX, a.START_DATE, a.END_DATE
+        ''',
+        '''
+        INSERT INTO data(TOTAL_INCARCERATION_LAST5YR, NUM_PREV_INCARCERATION_LAST5YR, AVG_INCARCERATION_LAST5YR)
+        SELECT
+        sum(b.INCARCERATION_LEN_DAYS) as TOTAL_INCARCERATION_LAST5YR,
+        count(*) as NUM_PREV_INCARCERATION_LAST5YR,
+        sum(b.INCARCERATION_LEN_DAYS)/count(*) as AVG_INCARCERATION_LAST5YR  
+        FROM data as a join data as b on a.ID=b.ID 
+        WHERE julianday(b.END_DATE) >= (julianday(a.END_DATE) - 1825)
+        GROUP BY a.ID, a.PREFIX, a.START_DATE, a.END_DATE
+        ''',
+        '''
+        INSERT INTO data(RELATIVE_INCARCERATION_LEN_DAYS)
+        SELECT 
+        INCARCERATION_LEN_DAYS - (SELECT
+        sum(b.INCARCERATION_LEN_DAYS)/count(*) as mean
+        FROM data as a, data as b
+        WHERE b.END_DATE <= a.END_DATE) as RELATIVE_INCARCERATION_LEN_DAYS
+        from data
+        '''
+        )
+
+    add_new_features(DATABASE_FILENAME, new_features, query)
+    print('created incarceration len features')
+    
 
 
 if __name__ == '__main__':
-    time_period = 365.0
-
-#    setup()  # To load database of data tables
-#    gettinglabels.create_labels(DATABASE_FILENAME, time_period=time_period, default_max = 10000.0, table_name = 'labels')  # Get labels
-    add_features() # Create new table data for features and data
-
-#    test_year=1997
-#    while test_year <= 2018:
-#        temporal_validation('test_'+ str(test_year), train_start_year=1995, test_start_year=test_year, time_period=time_period)
-#        test_year += 1
-
-
+    add_first_feature()
+    add_incarceration_lens()
 
